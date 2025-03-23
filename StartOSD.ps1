2ff7aa4e5788f9e40d66ecbf5cb2c4f9483ccb1a
VirtualCaffeineIO <virtualcaffeineio@gmail.com>
2025-03-23 08:58:08 -0700

    Create StartOSD.ps1

---
+++ StartOSD.ps1
@@ -0,0 +1,6 @@
+# zti.virtualcaffeine.io - needs to be here for blog post on akosbakos.ch
+
+Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
+Start-Sleep -Seconds 5
+
+Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Pro -OSLanguage en-us -OSLicense Retail
