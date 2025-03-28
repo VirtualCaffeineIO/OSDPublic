e45cc75d8efb2f9ce6c57b3d743848951b135993
VirtualCaffeineIO <virtualcaffeineio@gmail.com>
2025-03-28 08:52:55 -0700

    Update Prerequisites.ps1

--- Setup/host/Prerequisites.ps1
+++ Setup/host/Prerequisites.ps1
@@ -7,9 +7,9 @@ https://www.osdcloud.com/osdcloud/setup
 #>
 
 #region Install Microsoft ADK Windows 11 24H2 10.1.26100.1
-# Windows ADK 10.1.26100.1 (May 2024)
-$ADKUri = 'https://go.microsoft.com/fwlink/?linkid=2271337'
-Write-Host -ForegroundColor Green '[+] Downloading Windows ADK 10.1.26100.1 (May 2024) Setup from' $ADKUri
+# Windows ADK 10.1.25398.1 (Republished in January 2025)
+$ADKUri = 'https://go.microsoft.com/fwlink/?linkid=2243390'
+Write-Host -ForegroundColor Green '[+] Downloading Windows ADK 10.1.25398.1 (Republished in January 2025) Setup from' $ADKUri
 
 if ($host.name -match 'ConsoleHost') {
     Invoke-Expression "& curl.exe --insecure --location --output `"$env:TEMP\adksetup.exe`" --url `"$ADKUri`""
@@ -18,13 +18,13 @@ else {
     #PowerShell ISE will display a NativeCommandError, so progress will not be displayed
     $Quiet = Invoke-Expression "& curl.exe --insecure --location --output `"$env:TEMP\adksetup.exe`" --url `"$ADKUri`" 2>&1"
 }
-Write-Host -ForegroundColor Green '[+] Downloading and Installing Windows ADK 10.1.26100.1 (May 2024)'
+Write-Host -ForegroundColor Green '[+] Downloading and Installing Windows ADK 10.1.25398.1 (Republished in January 2025)'
 Start-Process -FilePath "$env:TEMP\adksetup.exe" -ArgumentList '/features', 'OptionId.DeploymentTools', '/quiet', '/ceip', 'off', '/norestart' -Wait
 
 
 # Windows PE add-on for the Windows ADK 10.1.26100.1 (May 2024)
-$WinPEUri = 'https://go.microsoft.com/fwlink/?linkid=2271338'
-Write-Host -ForegroundColor Green '[+] Downloading Windows PE add-on for the Windows ADK 10.1.26100.1 (May 2024) Setup from' $WinPEUri
+$WinPEUri = 'https://go.microsoft.com/fwlink/?linkid=2243391'
+Write-Host -ForegroundColor Green '[+] Downloading Windows PE add-on for the Windows ADK 10.1.25398.1 (Republished in January 2025) Setup from' $WinPEUri
 
 if ($host.name -match 'ConsoleHost') {
     Invoke-Expression "& curl.exe --insecure --location --output `"$env:TEMP\adkwinpesetup.exe`" --url `"$WinPEUri`""
@@ -33,7 +33,7 @@ else {
     #PowerShell ISE will display a NativeCommandError, so progress will not be displayed
     $Quiet = Invoke-Expression "& curl.exe --insecure --location --output `"$env:TEMP\adkwinpesetup.exe`" --url `"$WinPEUri`" 2>&1"
 }
-Write-Host -ForegroundColor Green '[+] Downloading and Installing Windows PE add-on for the Windows ADK 10.1.26100.1 (May 2024)'
+Write-Host -ForegroundColor Green '[+] Downloading and Installing Windows PE add-on for the Windows ADK 10.1.25398.1 (Republished in January 2025)'
 Start-Process -FilePath "$env:TEMP\adkwinpesetup.exe" -ArgumentList '/features', 'OptionId.WindowsPreinstallationEnvironment', '/quiet', '/ceip', 'off', '/norestart' -Wait
 
 # Bugfix for MDT Windows PE x86 MMC snap-in error
@@ -42,7 +42,7 @@ if (-not (Test-Path 'C:\Program Files (x86)\Windows Kits\10\Assessment and Deplo
 }
 
 # Complete
-Write-Host -ForegroundColor Green '[+] Windows ADK 10.1.26100.1 (May 2024) has been installed'
+Write-Host -ForegroundColor Green '[+] Windows ADK 10.1.25398.1 (Republished in January 2025) has been installed'
 #endregion
 
 #region Install Microsoft MDT 6.3.8456.1000
